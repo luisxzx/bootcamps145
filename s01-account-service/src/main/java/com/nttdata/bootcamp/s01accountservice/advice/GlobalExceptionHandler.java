354c8f946290bbdbd5e642afb6edcc9805187b43
luis <luis.rojas.chuchon.1@gmail.com>
2023-08-30 22:49:36 -0500

    proyectosemana1microservicios

---
+++ s01-account-service/src/main/java/com/nttdata/bootcamp/s01accountservice/advice/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.nttdata.bootcamp.s01accountservice.advice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.nttdata.bootcamp.s01accountservice.exception.AccountCreationException;
+import com.nttdata.bootcamp.s01accountservice.exception.AccountNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccountCreationException.class)
+    public ResponseEntity<ErrorResponse> handleAccountCreationException(AccountCreationException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleAccountNotFoundException(AccountNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    // Add more exception handlers as needed
+
+    // Custom error response class
+    private static class ErrorResponse {
+        private int status;
+        private String message;
+
+        public ErrorResponse(int status, String message) {
+            this.status = status;
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
